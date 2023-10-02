b5a6f003d1a247d7edf5dca047cae6cea1a84675
Daud Shah <sdaud4214@gmail.com>
2023-10-02 05:48:53 -0600

    python

---
+++ day_3/01_first_program.py
@@ -0,0 +1,3 @@
+print("Daud shah")
+print(12+34)
+print("i leran python for Ai ")
---
+++ day_3/02_operater.py
@@ -0,0 +1,7 @@
+print(2+7)
+print(8-6)
+print(8*4)
+print(8/4)#you will get a float number
+print(8//4)#you will get a int number
+print(8%9)#remaainder after division
+print(8**5)#exponent 8x8x8x8x8
